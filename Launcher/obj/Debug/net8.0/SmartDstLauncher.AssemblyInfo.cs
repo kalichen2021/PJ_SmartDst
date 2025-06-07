d13735ad45a87a3dbc600d41c2371078ea01772a
kalichen2021 <kalichen_2021@outlook.com>
2025-06-07 10:48:43 +0800

    chore: 更新程序集信息缓存和版本号

--- Launcher/obj/Debug/net8.0/SmartDstLauncher.AssemblyInfo.cs
+++ Launcher/obj/Debug/net8.0/SmartDstLauncher.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SmartDstLauncher")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+88f6a0284d49c044a3b9d4eee1777dd5717224f7")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+552292bfd9be9965f9a64c2f23bed8eeeaf65c3a")]
 [assembly: System.Reflection.AssemblyProductAttribute("SmartDstLauncher")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SmartDstLauncher")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
