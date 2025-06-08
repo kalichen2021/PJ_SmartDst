8acffbb371497ee4634b84226aa3cb2abf9297d2
kalichen2021 <kalichen_2021@outlook.com>
2025-06-08 22:54:15 +0800

    refactor(stores): 重构用户操作存储以支持多图标组
    将简单的图标位置和大小状态重构为支持多组图标的类结构，使用reactive和computed实现响应式多边形计算

--- src/stores/UserOpera.ts
+++ src/stores/UserOpera.ts
@@ -1,27 +1,54 @@
-import type { Point, Polygon } from "@/assets/js/type";
+import type { iconGroupClass, Point, Polygon } from "@/assets/js/type";
 import { rectToPolygon } from "@/assets/js/utils";
 import { defineStore } from "pinia";
-import { ref, type Ref } from "vue";
+import { computed, reactive, ref, type Ref } from "vue";
 
 
 export const UseUserOperaStore = defineStore('userOpera', () => {
   const ctrlState: Ref<"MOVE" | "SCALE" | "IDLE" | "EDIT"> = ref("IDLE")
-  const iconGroupPosition: Ref<Point> = ref([0, 0])
-  const iconGroupSize: Ref<Point> = ref([3, 3])
+  // const iconGroupPosition: Ref<Point> = ref([0, 0])
+  // const iconGroupSize: Ref<Point> = ref([3, 3])
   const canvasAnimate: Ref<(curPosition: Point) => void> = ref(() => { })
   const initializeParticles: Ref<Function> = ref(() => { })
 
-  const getIconGroupPolygon = (): Polygon => {
-    return rectToPolygon({
-      x: iconGroupPosition.value[0],
-      y: iconGroupPosition.value[1],
-      width: iconGroupSize.value[0],
-      height: iconGroupSize.value[1]
-    })
+  const iconGroupClass: Ref<iconGroupClass> = ref([
+    {
+      name: "default",
+      iconGroupPosition: [0, 0],
+      iconGroupSize: [3, 3],
+      position: reactive({
+        x: 0,
+        y: 0,
+        width: 3,
+        height: 3
+      }),
+      rangePolygon: computed(() => rectToPolygon({
+        x: position.x,
+        y: position.y,
+        width: position.width,
+        height: position.height
+      }))
+    }
+  ])
+  // const getIconGroupClass = (): iconGroupClass => {
+  //   return []
+  // }
 
-  }
+  // const getIconGroupPolygon = (): Polygon => {
+  //   return rectToPolygon({
+  //     x: iconGroupPosition.value[0],
+  //     y: iconGroupPosition.value[1],
+  //     width: iconGroupSize.value[0],
+  //     height: iconGroupSize.value[1]
+  //   })
+
+  // }
 
   return {
-    ctrlState, iconGroupPosition, iconGroupSize, canvasAnimate, initializeParticles
+    ctrlState,
+    // iconGroupPosition,
+    // iconGroupSize, 
+    canvasAnimate, initializeParticles,
+    // getIconGroupPolygon
   }
 })
